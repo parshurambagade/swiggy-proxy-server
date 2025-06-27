99e4e4e19504b80b6013b9e8eb589780f6196ca2
Parshuram Bagade <parshurambagade855@gmail.com>
2025-06-27 22:17:22 +0530

    feat(/api/restaurant): add api to handle restaurant info

--- constants.js
+++ constants.js
@@ -3,6 +3,9 @@ export const SWIGGY_API_URL =
 export const SWIGGY_MOBILE_API_URL =
   "https://www.swiggy.com/mapi/restaurants/list/v5?is-seo-homepage-enabled=true";
 
+export const SWIGGY_RESTAURANT_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&restaurantId=";
+
 export const MOBILE_HEADERS = {
   "User-Agent":
     "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.61 Mobile Safari/537.36",
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import {
   SWIGGY_MOBILE_API_URL,
   MOBILE_HEADERS,
   DESKTOP_HEADERS,
+  SWIGGY_RESTAURANT_URL,
 } from "./constants.js";
 import express from "express";
 import cors from "cors";
@@ -10,7 +11,7 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
-app.get("/api/swiggy", async (req, res) => {
+app.get("/api/restaurants/nearby", async (req, res) => {
   const { device, lat, lng } = req.query;
 
   if (!device || !lat || !lng) {
@@ -38,6 +39,33 @@ app.get("/api/swiggy", async (req, res) => {
         .status(response.status)
         .json({ error: "Unexpected response", contentType });
     }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Proxy failed", detail: err.message });
+  }
+});
+
+app.get("/api/restaurant", async (req, res) => {
+  const { resId, lat, lng } = req.query;
+
+  if (!resId) {
+    return res.status(400).json({ error: "Missing restaurant ID" });
+  }
+
+  const URL = SWIGGY_RESTAURANT_URL + resId;
+
+  try {
+    const response = await fetch(`${URL}&lat=${lat}&lng=${lng}`, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+        Accept: "application/json",
+        Referer: "https://www.swiggy.com/",
+        Origin: "https://www.swiggy.com",
+      },
+    });
 
     const data = await response.json();
     res.status(200).json(data);
