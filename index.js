12e5af04ddd3ae910c2f503af451ce48ac7f0440
Parshuram Bagade <parshurambagade855@gmail.com>
2025-06-27 16:08:13 +0530

    add(script): add start script

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ app.get("/api/swiggy", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-  console.log(`Proxy server running on http://localhost:${PORT}`);
+  console.log(`Proxy server running on the port: ${PORT}`);
 });
